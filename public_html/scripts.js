d7dbecfe7a61184d1fb9c9fa078afc8c01ab184f
Ivan Salgado <ivansalgado2000@gmail.com>
2015-01-09 09:16:13 -0800

    fixed my problem

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,3 +1,9 @@
 $("document").ready(function(){
-    $(".ivan").css("background-color", "pink");
+    $(".ivan").css("background-color", "green");
+
+    $(".btn-group").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
 });
+
+$('.btn-group').bind(function(mouseOverMe){
+    $('btn-group').fadeIn(2000);
+});    
